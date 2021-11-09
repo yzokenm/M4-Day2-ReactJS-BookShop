5cd3506f1b5bd5447e8ed9a68ff01195ebe6ebd3
alimukhtor <james1999007@gmail.com>
2021-11-09 15:03:57 +0100

    added navbar to the page

--- react-day2/src/App.js
+++ react-day2/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './components/MyNavbar'
+import MyFooter from './components/MyFooter'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <MyNavbar />
+    <MyFooter />
     </div>
   );
 }
